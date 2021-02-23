1dc33a82cabd5c79d05197673bc6c1c27f69ace9
Eric <eric.alexander.lawson@gmail.com>
2021-02-22 19:34:13 -0800

    added function to download images using axios and save them to a file. Added the output folder to gitignore

--- src/scrape.ts
+++ src/scrape.ts
@@ -2,11 +2,21 @@ import { fstat } from 'fs';
 import mongodb from 'mongodb';
 import fs from 'fs';
 import scrapeWiki from './scrapeWiki';
+import parseGod from './parseGod';
+import axios, { AxiosResponse } from 'axios';
+import God from 'smite-timeline/src/data_objects/God';
+import Item from 'smite-timeline/src/data_objects/Item';
+import { resolve } from 'path';
+import { rejects } from 'assert';
+import parseItem from './parseItem';
 
 //Webscraper Main Entrypoint
 (async function() {
-    const {items, gods} = await scrapeWiki();
-    writeRawHtmlToDatabase(gods, items);
+    const {items: itemsRaw, gods: godsRaw} = await scrapeWiki();
+    writeRawHtmlToDatabase(godsRaw, itemsRaw);
+    const parsedGods = godsRaw.map(godRaw => parseGod(godRaw.html));
+    const parsedItems = itemsRaw.map(itemRaw => parseItem(itemRaw.html));
+    downloadImages(parsedGods, parsedItems);
 })();
 
 export type RawHTML = {
@@ -25,5 +35,19 @@ function writeRawHtmlToDatabase(gods: RawHTML[], items: RawHTML[]) {
         console.log(collection);
         // perform actions on the collection object
         client.close();
+    });
+}
+
+async function downloadImages(gods: God[], items: Item[]) {
+    gods.map(async god => {
+        const response: AxiosResponse<fs.ReadStream> = await axios(god.image, {responseType:'stream'});
+        response.data.pipe(fs.createWriteStream('./images/god-cards/' + god.name));
+    });
+    items.map(async item => {
+        const response: AxiosResponse<fs.ReadStream> = await axios(item.image, { responseType: 'stream' });
+        const stream = response.data.pipe(fs.createWriteStream('./images/items/' + item.name));
+        stream
+            .on('finish', () => {return;})
+            .on('error', err => {throw err});
     });
 }
