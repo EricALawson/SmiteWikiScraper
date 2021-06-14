9ea0a8eb350b9bf6b64932f1a7d6325f518f28a5
Eric <eric.alexander.lawson@gmail.com>
2021-06-13 23:21:31 -0700

    Refactored writeTodatabase so it can recieve login strings as parameters, which can be used to call tests on a test database.

--- src/writeToDatabase.ts
+++ src/writeToDatabase.ts
@@ -5,16 +5,22 @@ import { ParseResult } from './ScrapeTarget';
 
 const loginFile = './dblogin.txt';
 const login = fs.readFileSync(loginFile).toString().split('\n');
-const [name, pass, database] = login.map(str => str.trim());
-const uri = `mongodb+srv://${name}:${pass}@smite-timeline-0.n3o8x.mongodb.net/${database}?retryWrites=true&w=majority`;
+const [nm, ps, db] = login.map(str => str.trim());
 
-export async function writeToDatabase(parseResult: ParseResult | Error): Promise<ParseResult | Error> {
+
+export async function writeToDatabase(
+    parseResult: ParseResult | Error,
+    name: string = nm, 
+    pass: string = ps, 
+    database: string = db
+): Promise<ParseResult | Error> {
+    const uri = `mongodb+srv://${name}:${pass}@smite-timeline-0.n3o8x.mongodb.net/${database}?retryWrites=true&w=majority`
     if (parseResult instanceof Error)
         return parseResult;
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     try {
         await client.connect();
-        const db = client.db('smite-timeline');
+        const db = client.db(database);
         const rawHTMLCollection = db.collection("wiki-scraper-raw");
         const updateHTMLResult = await rawHTMLCollection.updateOne(
             {
@@ -35,6 +41,9 @@ export async function writeToDatabase(parseResult: ParseResult | Error): Promise
             { $set: parseResult.parseResult},
             { upsert: true }
         );
+        if (updateParsedResult.result.ok < updateParsedResult.result.n) {
+            throw new Error('a write operation failed');
+        }
         
     } catch (err) {
         return err;
